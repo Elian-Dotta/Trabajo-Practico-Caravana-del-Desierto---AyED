c173e360d647f835df1fa6ece5ce60a333e1cafe
Manuel Perez <manuperezpuente@gmail.com>
2026-05-11 20:05:46 -0300

    Arreglo la rama

---
+++ files/jugador.h
@@ -0,0 +1,6 @@
+#ifndef JUGADOR_H_
+#define JUGADOR_H_
+
+
+
+#endif // JUGADOR_H_
---
+++ files/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include "config.h"
+
+int main()
+{
+    printf("Caravana Del Desierto\n");
+    //LLAMA A MENU
+
+    return 0;
+}
